7404c9092fda1a67e0febeb1031cda0587e8f3ed
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2019-08-13 17:21:37 +0200

    Dodanie modeulu i routingu Contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @routing		GET api/contacts
 // @opis			Pobranie wszystkich kontaktów użytkowników
 // @dostęp		Prywatny
-router.get('/', (req, res) => {
-	res.send('Pobrano wszystkie kontakty');
+router.get('/', auth, async (req, res) => {
+	try {
+		const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+		res.json(contacts);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Błąd serwera');
+	}
 });
 
 // @routing		Post api/contacts
