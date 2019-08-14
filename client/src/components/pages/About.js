1e78e8f41c38d9f1c8338faef0a3ba83e1aec589
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2019-08-15 00:01:17 +0200

    Dodanie nawigacji, contextu, typów do reducera i globalnego state'a

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<div>
+			<h1>O Aplikacji</h1>
+			<p className="my-1">
+				To jest full-stackowa reactowa aplikacja do zapisywania kontaktów
+			</p>
+			<p className="bg-dark p">
+				<strong>Wersja: </strong> 1.0.0
+			</p>
+		</div>
+	)
+}
+
+export default About
